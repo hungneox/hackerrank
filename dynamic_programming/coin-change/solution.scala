f91ba00318e5f0162be5ef12cb3db35411735f6e
Q.Hung <hungnq1989@gmail.com>
2017-02-21 00:19:58 +0200

    Complete solution.scala fix data type bug

--- dynamic_programming/coin-change/solution.scala
+++ dynamic_programming/coin-change/solution.scala
@@ -2,20 +2,21 @@ import scala.io.StdIn.{readLine,readInt}
 import collection.mutable.HashMap
 
 object Solution {
-    def makeChange(coins: Array[Int], money: Int, index: Int, memo: HashMap[String, Int]) : Int = {
+    def makeChange(coins: Array[Int], money: Long, index: Int, memo: HashMap[String, Long]) : Long = {
         if (money == 0) return 1
         if (index >= coins.length) return 0
 
         val key = money.toString + '-' + index.toString
-        if (memo.contains(key)) {
+
+        if (memo.keySet.contains(key)) {
             return memo.getOrElse(key, 0)
         }
-        var amountWithCoins = 0
-        var ways = 0
+        var amountWithCoins: Long = 0
+        var ways: Long = 0
         while (amountWithCoins <= money) {
             val remaining = money - amountWithCoins
             ways = ways + this.makeChange(coins, remaining, index+1, memo)
-            amountWithCoins = amountWithCoins + coins(index)
+            amountWithCoins += coins(index)
         }
         memo.put(key, ways)
         return ways
@@ -26,7 +27,7 @@ object Solution {
         val money = first(0)
         val M = first(1)
         val coins = readLine.split(" ").map(_.toInt)
-        val memo = HashMap[String, Int]()
+        val memo = HashMap[String, Long]()
         var result = this.makeChange(coins, money, 0, memo)
         println(result)
   }
