3a69d4aaeefb18603fe2c28bafa75826b9611edf
Q.Hung <hungnq1989@gmail.com>
2017-02-21 00:35:15 +0200

    Update comment for camelcase

--- strings/camelcase/solution.py
+++ strings/camelcase/solution.py
@@ -2,6 +2,14 @@
 # https://www.hackerrank.com/challenges/camelcase
 import sys
 
+# input: saveChangesInTheEditor
+# 1. save
+# 2. Changes
+# 3. In
+# 4. The
+# 5. Editor
+# output: 5
+
 s = raw_input().strip()
 
 s = s[0].upper() + s[1:]
