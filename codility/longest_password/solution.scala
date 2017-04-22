47a932903f7d8feffba06ce95da2240d094ce7d4
Q.Hung <hungnq1989@gmail.com>
2017-04-23 02:31:47 +0300

    Fix bug: return -1 when result = 0

--- codility/longest_password/solution.scala
+++ codility/longest_password/solution.scala
@@ -1,5 +1,10 @@
 import scala.collection.JavaConverters._
 
+// it has to contain only alphanumerical characters (a-z, A-Z, 0-9);
+// there should be an even number of letters;
+// there should be an odd number of digits.
+// for (i <- List.range(1, 10) if i % 2 == 0) yield i
+
 object Solution {
   def solution(s: String): Int = {
     // write your code in Scala 2.12
@@ -10,9 +15,11 @@ object Solution {
         var letters = for (w <- words(i) if w.isLetter) yield w
         var length = digits.length + letters.length
         if (length == words(i).length){
-            maxLength = Math.max(length, maxLength)
+            if(digits.length % 2 ==1 && letters.length % 2 == 0){
+                maxLength = Math.max(length, maxLength)
+            }
         }   
     }
-    return maxLength
+    return if (maxLength > 0) maxLength else -1
   }
 }
